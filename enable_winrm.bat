699d664d2b972807713c54b6ed2cfd17d0b5ecda
Satoru Baba <sbaba@jp.ibm.com>
2018-08-24 19:26:08 +0900

    add enable_winrm.bat add get-installer.sh

---
+++ enable_winrm.bat
@@ -0,0 +1,8 @@
+ECHO Start-Transcript -path "C:\Users\Administrator\Desktop\enable_winrm.log" -append >>C:\Users\Administrator\Desktop\enable_winrm.ps1
+ECHO Invoke-WebRequest -Uri https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 -OutFile ConfigureRemotingForAnsible.ps1
+REM ECHO powershell -ExecutionPolicy RemoteSigned .\ConfigureRemotingForAnsible.ps1
+ECHO powershell -ExecutionPolicy RemoteSigned .\ConfigureRemotingForAnsible.ps1 -SkipNetworkProfileCheck
+
+ECHO Stop-Transcript >>C:\Users\Administrator\Desktop\enable_winrm.ps1
+ECHO Restart-Computer -Force >> C:\Users\Administrator\Desktop\enable_winrm.ps1
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Unrestricted C:\Users\Administrator\Desktop\enable_winrm.ps1
---
+++ files/get-installers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -nd http://fg.v4.download.windowsupdate.com/c/msdownload/update/software/updt/2016/09/lp_9a666295ebc1052c4c5ffbfa18368dfddebcd69a.cab
+wget -nd https://s3.amazonaws.com/aws-cli/AWSCLI64.msi
+
