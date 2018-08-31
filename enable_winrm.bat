6f8cc259cc18e808a4a2d062921def172cd6152c
Satoru Baba <sbaba@jp.ibm.com>
2018-08-31 17:01:47 +0900

    add changing network-type for private NIC.

--- enable_winrm.bat
+++ enable_winrm.bat
@@ -1,7 +1,8 @@
 ECHO Start-Transcript -path "C:\Users\Administrator\Desktop\enable_winrm.log" -append >>C:\Users\Administrator\Desktop\enable_winrm.ps1
 ECHO Invoke-WebRequest -Uri https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 -OutFile ConfigureRemotingForAnsible.ps1 >>C:\Users\Administrator\Desktop\enable_winrm.ps1
+ECHO Get-NetConnectionProfile | where InterfaceAlias -eq PrivateNetwork-A| Set-NetConnectionProfile -NetworkCategory Private >>C:\Users\Administrator\Desktop\enable_winrm.ps1 
+ECHO Get-NetConnectionProfile  >>C:\Users\Administrator\Desktop\enable_winrm.ps1
 ECHO powershell -ExecutionPolicy RemoteSigned .\ConfigureRemotingForAnsible.ps1 -SkipNetworkProfileCheck >>C:\Users\Administrator\Desktop\enable_winrm.ps1
-
 ECHO Stop-Transcript >>C:\Users\Administrator\Desktop\enable_winrm.ps1
 ECHO Restart-Computer -Force >> C:\Users\Administrator\Desktop\enable_winrm.ps1
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Unrestricted C:\Users\Administrator\Desktop\enable_winrm.ps1
