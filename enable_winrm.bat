b5d68491dc5317b0baa95aa3366bc7cef0e50e43
Satoru Baba <sbaba@jp.ibm.com>
2018-09-03 14:10:30 +0900

    update

--- enable_winrm.bat
+++ enable_winrm.bat
@@ -1,6 +1,6 @@
 ECHO Start-Transcript -path "C:\Users\Administrator\Desktop\enable_winrm.log" -append >>C:\Users\Administrator\Desktop\enable_winrm.ps1
 ECHO Invoke-WebRequest -Uri https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 -OutFile ConfigureRemotingForAnsible.ps1 >>C:\Users\Administrator\Desktop\enable_winrm.ps1
-ECHO "Get-NetConnectionProfile | where InterfaceAlias -eq PrivateNetwork-A| Set-NetConnectionProfile -NetworkCategory Private" >>C:\Users\Administrator\Desktop\enable_winrm.ps1 
+ECHO "Get-NetConnectionProfile | where InterfaceAlias -eq PrivateNetwork-A | Set-NetConnectionProfile -NetworkCategory Private" >>C:\Users\Administrator\Desktop\enable_winrm.ps1 
 ECHO Get-NetConnectionProfile  >>C:\Users\Administrator\Desktop\enable_winrm.ps1
 ECHO powershell -ExecutionPolicy RemoteSigned .\ConfigureRemotingForAnsible.ps1 -SkipNetworkProfileCheck >>C:\Users\Administrator\Desktop\enable_winrm.ps1
 ECHO Stop-Transcript >>C:\Users\Administrator\Desktop\enable_winrm.ps1
